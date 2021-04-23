a0e87a4c45d6b5bc4a714151ce90a708d23c2298
big-viktor <79458452+big-viktor@users.noreply.github.com>
2021-04-23 15:56:01 +0300

    automation of saved styles

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,15 @@ let gulp = require('gulp');
 let rename = require('gulp-rename');
 let sass = require('gulp-sass');
 let autoprefixer = require('gulp-autoprefixer');
+let sourcemaps = require('gulp-sourcemaps');
+
 
 
 
 function css_style (done){
 
-    gulp.src('./scss/style.scss')
+    gulp.src('./scss/**/*.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass({
             errorLogToConsole: true,
             outputStyle: 'compressed'
@@ -18,13 +21,25 @@ function css_style (done){
             cascade: true
         }))
         .pipe(rename({suffix: '.min'}))
+        .pipe(sourcemaps.write('./'))
         .pipe( gulp.dest('./css/') );
 
     done();
 }
 
+function print(done) {
+    console.log("hi");
+    done();
+}
 
+function watchSass(){
+    gulp.watch("./scss/**/*",css_style);
+}
 
-gulp.task(css_style);
+// gulp.task(css_style);
+// gulp.task(print);
 
-exports.default = css_style;
+gulp.task('default',gulp.series(print, watchSass));
+
+// exports.default = css_style;
+// exports.default = print;
