c7a2c3c4cd65d6cd7118969fe3a54d3c0699704e
big-viktor <79458452+big-viktor@users.noreply.github.com>
2021-04-25 17:49:07 +0300

    photo minimization

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,14 @@
+
 let project_folder = "dist";
 let source_folder = "app";
 
 let path = {
     build: {
         html: project_folder + "/",
-        css: project_folder + "/css",
-        js: project_folder + "/js",
-        img: project_folder + "/img",
-        fonts: project_folder + "/fonts"
+        css: project_folder + "/css/",
+        js: project_folder + "/js/",
+        img: project_folder + "/img/",
+        fonts: project_folder + "/fonts/"
     }, 
     src: {
         html: [source_folder + "/*.html", "!" + source_folder + "/_*.html"],
@@ -20,7 +21,7 @@ let path = {
         html: source_folder + "/**/*.html",
         css: source_folder + "/scss/**/*.scss",
         js: source_folder + "/js/**/*.js",
-        img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
+        img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}"
     },
     clean: "./" + project_folder + "/"
 };
@@ -30,8 +31,13 @@ let path = {
     browsersync = require("browser-sync").create(),
     fileinclude = require("gulp-file-include"),
     del = require("del"),
-    scss = require("gulp-sass");
-
+    scss = require("gulp-sass"),
+    autoprefixer = require("gulp-autoprefixer"),
+    group_media = require("gulp-group-css-media-queries"),
+    clean_css = require("gulp-clean-css"),
+    rename = require("gulp-rename"),
+    uglify = require("gulp-uglify-es").default,
+    imagemin = require("gulp-imagemin");
 
 
 function browserSync (params) {
@@ -50,11 +56,68 @@ function html() {
     .pipe(dest(path.build.html))
     .pipe(browsersync.stream())
 }
+function css() {
+    // получения ісходного коду
+    return src(path.src.css)
+    // оброботка scss 
+    .pipe(
+        scss({
+            outputStyle:'expanded'
+        })
+    )
+    .pipe(
+        group_media()
+    )
+    .pipe(
+        autoprefixer({
+            overrideBrowserslist:["last 5 version"],
+            cascade: true
+        })
+    )
+    .pipe(dest(path.build.css))
+    .pipe(clean_css())
+    .pipe(
+        rename({
+            extname:".min.css"
+        })
+    )
+    .pipe(dest(path.build.css))
+    .pipe(browsersync.stream())
+}
+
+function js() {
+    return src(path.src.js)
+    .pipe(fileinclude())
+    .pipe(dest(path.build.js))
+    .pipe(
+        uglify()
+    )
+    .pipe(
+        rename({
+            extname:".min.js"
+        })
+    )
+    .pipe(dest(path.build.js))
+    .pipe(browsersync.stream())
+}
+
+function images() {
+    return src(path.src.img)
+    .pipe(
+        imagemin()
+    )
+    .pipe(dest(path.build.img))
+    .pipe(browsersync.stream())
+}
 
 function watchFiles(params) {
     // слежка файлів при ізменению 
     gulp.watch([path.watch.html], html);
     gulp.watch([path.watch.css], css);
+    gulp.watch([path.watch.js], js);
+    gulp.watch([path.watch.img], images);
+
+
 }
 
 function clean(params) {
@@ -62,20 +125,13 @@ function clean(params) {
     return del(path.clean);
 }
 
-function css() {
-    return src(path.src.css)
-    .pipe(
-        scss({
-            outputStyle:'expanded'
-        })
-    )
-    .pipe(dest(path.build.css))
-    .pipe(browsersync.stream())
-}
 
-let build = gulp.series(clean, gulp.parallel( css, html));
-let watch = gulp.parallel(build,watchFiles, browserSync);
 
+let build = gulp.series(clean, gulp.parallel( css, html, images, js ));
+let watch = gulp.parallel(build, watchFiles, browserSync);
+
+exports.images = images;
+exports.js = js;
 exports.css = css;
 exports.html = html;
 exports.build = build;
